b1317ada8a27cf588be8a94218655f94531a6fff
taadis <i@taadis.com>
2022-05-25 01:50:44 +0800

    upgrade go-micro v3

--- go.mod
+++ go.mod
@@ -1,12 +1,10 @@
-module github.com/taadis/gateway-service
+module github.com/taadis/gateway-api
 
 go 1.14
 
 require (
+	github.com/asim/go-micro/v3 v3.7.1
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
-	github.com/micro/go-micro v1.18.0
-	github.com/micro/micro v1.18.0
 )
 
 replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"os"
 
+	"github.com/asim/go-micro/v3/cmd"
 	"github.com/golang/glog"
-	"github.com/micro/micro/cmd"
 )
 
 func main() {
@@ -15,14 +15,15 @@ func main() {
 	os.Setenv("MICRO_REGISTRY", "etcd")
 	os.Setenv("MICRO_REGISTRY_ADDRESS", "etcd:2379")
 
-	cmd.Init(
-		//micro.Name("wpt.api"),
-		//micro.Registry(
-		//	etcd.NewRegistry(
-		//		registry.Addrs("127.0.0.1:2379"),
-		//	),
-		//),
-	)
+	cmd.Init()
+	//cmd.Init(
+	//	micro.Name("wpt.api"),
+	//	micro.Registry(
+	//		etcd.NewRegistry(
+	//			registry.Addrs("127.0.0.1:2379"),
+	//		),
+	//	),
+	//)
 
 	//if err := run(); err != nil {
 	//	glog.Fatal(err)
